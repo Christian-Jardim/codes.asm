c92d907fa76ef53c2134f463ad883dc1f5bf9f4d
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-06-10 22:32:09 -0300

    Update teste_i-j.asm

--- teste_i-j.asm
+++ teste_i-j.asm
@@ -1,37 +1,30 @@
-li $t0, 1
-li $t1, 2
-sw $t0,0($zero)
-sw $t0,4($zero)
-sw $t0,8($zero)
-sw $t2,12($zero)
-
 main:   
-li      $t1, 2              
-li     $t2, 0          
-li      $t0, 0x0                
-addi    $t3, $zero, 0           
-addi    $t6, $zero, 0           
-addi    $t8, $zero, 4           
+addi    $t1, $zero, 100       # N = 100       
+addi    $t2, $zero, 0         # j = 0
+addi    $t0, $zero, 0x0       # endereço base (matriz) = 0      
+addi    $t3, $zero, 0         # i = 0
+addi    $t6, $zero, 0         # sum = 0
+addi    $t8, $zero, 4         # base = 4
 
 loop:
-beq $t2, $t1, loop_i
-mul     $t4, $t1, $t8           
-mul     $t4, $t4, $t2           
-mul     $t7, $t3, $t8           
-add     $t4, $t4, $t7            
-add     $t4, $t4, $t0
-lw      $t5, 0($t4)             
-
-add     $t6, $t6, $t5           
-addi    $t2, $t2, 1            
-j       loop                    
+beq     $t3, $t1, end         # se i = 100, finaliza o programa
+beq     $t2, $t1, loop_i      # se j = 0, vai para o loop_i
+mul     $t4, $t1, $t8         # $t4 = N * base
+mul     $t4, $t4, $t2         # $t4 = $t4 * j
+mul     $t7, $t3, $t8         # $t7 = i * base
+add     $t4, $t4, $t7         # Calculo do deslocamento
+add     $t4, $t4, $t0         # endereço do dado = deslocamento + endereço base (matriz)
+lw        $t5, 0($t4)         # Carrega o valor do indereço para $t5  
+add     $t6, $t6, $t5         # sum = sum + $t5
+addi    $t2, $t2, 1           # j = j +  1
+j       loop                  # Vai para o loop
 
 loop_i: 
-beq $t3, $t1, end
-addi $t3, $t3, 1
-li $t2, 0
-j loop
+beq $t3, $t1, end             # se i = 100, finaliza o programa
+addi $t3, $t3, 1              # i = i + 1
+li $t2, 0                     # j = 0
+j loop                        # Vai para o loop
 
 end:    
-li      $v0, 10               
+li      $v0, 10               # Finaliza o programa
 syscall
